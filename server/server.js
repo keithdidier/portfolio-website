089706e8b224d978eef37d3f10322cbd6b3844b0
keithdidier <keith.b.didier@gmail.com>
2017-12-02 14:17:38 -0700

    require path

--- server/server.js
+++ server/server.js
@@ -5,5 +5,10 @@ const app = express();
 
 app.use(bodyParser);
 
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
 const PORT = 8082;
 app.listen(() => console.log(`Server listening on port: ${PORT}`));
