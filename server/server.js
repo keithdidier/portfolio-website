4f777c1b7d60608d04278694b3ac2f1cafeb201d
keithdidier <keith.b.didier@gmail.com>
2017-12-04 15:51:47 -0700

    changed port

--- server/server.js
+++ server/server.js
@@ -12,5 +12,5 @@ app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-const PORT = 3006;
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
