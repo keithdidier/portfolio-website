2a5abe0b40eefc6e9e126c8730faa6a1c12a3b53
keithdidier <keith.b.didier@gmail.com>
2017-12-04 15:51:05 -0700

    fixed port

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use( express.static( `${__dirname}/../build` ));
 
-app.use(bodyParser);
+app.use(bodyParser.json());
 
 const path = require('path')
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-const PORT = 8080;
-app.listen(() => console.log(`Server listening on port: ${PORT}`));
+const PORT = 3006;
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
