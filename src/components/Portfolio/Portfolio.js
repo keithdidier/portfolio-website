38e55910ed1f061c10af132d83d949be05349ccf
keithdidier <keith.b.didier@gmail.com>
2017-12-20 12:44:44 -0500

    redesigned

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -11,20 +11,22 @@ export default class Portfolio extends Component {
                     <h1>Recent Work</h1>
                 </div>
                 <section className="projects">
-                <div className="img-container">
-                        <a href="http://104.236.218.72:3010/#/" target="_blank">
-                            <img src={rbcScreenShot} className="rbc-screen-shot"/>
-                        </a>
+                    <div className="showcase">
+                        <div className="img-container">
+                            <a href="http://104.236.218.72:3010/#/" target="_blank">
+                                <img src={rbcScreenShot} className="rbc-screen-shot"/>
+                            </a>
                         </div>
-                    <div className="description">
-                        <h3><a href="http://104.236.218.72:3010/#/" target="_blank">Redbubble Clone</a></h3>
-                        <p>
-                            There are many variations of passages of Lorem Ipsum available,<br/>
-                            but the majority have suffered alteration in some form,<br/>
-                            by injected humour, or randomised words which don't look even<br/>
-                            slightly believable. If you are going to use a passage of Lorem<br/>
-                            Ipsum, you need to be sure there isn't anything embarrassing<br/>
-                        </p>
+                        <div className="description">
+                            <h3><a href="http://104.236.218.72:3010/#/" target="_blank">Redbubble Clone</a></h3>
+                            <p>
+                                There are many variations of passages of Lorem Ipsum available,<br/>
+                                but the majority have suffered alteration in some form,<br/>
+                                by injected humour, or randomised words which don't look even<br/>
+                                slightly believable. If you are going to use a passage of Lorem<br/>
+                                Ipsum, you need to be sure there isn't anything embarrassing<br/>
+                            </p>
+                        </div>
                     </div>
                 </section>
             </section>
