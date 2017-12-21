ed034b407fe89797b2e2c735410dd8be45725265
keithdidier <keith.b.didier@gmail.com>
2017-12-20 20:48:32 -0500

    working on contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,8 @@ export default class Contact extends Component {
         super();
 
         this.state = {
-            open: false
+            open: false,
+            value: ""
         }
     }
 
@@ -23,6 +24,31 @@ export default class Contact extends Component {
         });
     };
 
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        this.handleTouchTap();
+        event.preventDefault();
+    };
+
+    // handleInput = (input, value) => {
+    //     this.setState({
+    //         input: value
+    //     });
+    //     console.log(input);
+    // };
+
+    // isInputGiven = () => {
+    //     if(this.handleInput > 0) {
+    //         alert("Submited")
+    //         console.log(this.state.handleInput)
+    //     };
+    // };
+
     render() {
         return(
             <section id="contact-wrapper">
@@ -30,27 +56,32 @@ export default class Contact extends Component {
                     <h1>Contact</h1>
                 </div>
                 <section className="contact-para">
-                    <p>If you would like to contact me,
-                        feel free to send a message in the form to the left.
+                    <p>If you would like to contact me with any questions or opportunities,
+                        please feel free to send a message.
                         You are also welcome to contact me through LinkedIn. 
                         <a href="https://www.linkedin.com/in/keithdidier/" target="_blank">
-                            <i className="fa fa-angle-right fa-lg" aria-hidden="true"></i>
+                            <i className="fa fa-external-link" aria-hidden="true"></i>
                         </a>   
                     </p>
                 </section>
-                <form className="post-message">
-                    <input type="text" placeholder=" Name"/>
-                    <input type="text" placeholder=" Email"/>
-                    <input type="text" placeholder=" Phone"/>
-                    <input type="text" placeholder=" Company Name"/>
+                <form className="post-message" onSubmit={this.handleSubmit}>
+                    <span>Your Name</span>
+                    <input type="text" required onChange={this.handleChange}/>
+                    <span>Your Email</span>
+                    <input type="text" required/>
+                    <span>Your Phone Number(optional)</span>
+                    <input type="text"/>
+                    <span>Subject</span>
+                    <input type="text"/>
+                    <span>Message</span>
                     <textarea 
                     id="write-message" 
                     rows="8" cols="50" 
-                    placeholder=" Message"></textarea>
+                    ></textarea>
+                    <div className="send-message">
+                        <button onClick={this.isInputGiven} type="submit">Send</button>
+                    </div>
                 </form>
-                <div className="send-message">
-                    <button onClick={this.handleTouchTap} type="submit">Send</button>
-                </div>
                 <MuiThemeProvider>
                     <Snackbar
                     open={this.state.open}
